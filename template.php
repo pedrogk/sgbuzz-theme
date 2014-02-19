5cbdc9380467cba2cb795125f4781809c03d6b1e
root <root@sg.com.mx>
2014-02-18 23:11:14 -0500

    Edité template.php para cambiar more link de aggregator. Ver issue sgbuzz-25.

--- template.php
+++ template.php
@@ -5,3 +5,17 @@
  * Template overrides as well as (pre-)process and alter hooks for the
  * sgbuzz theme.
  */
+
+
+function sgbuzz_more_link ($array)
+{
+   if (stristr( $array['url'], 'aggregator'))
+   {
+      return "<div class='more-link'>"
+            ."    <a href='http://talento.sg.com.mx/vacantes/show.php' target='_blank'>Más vacantes</a>"
+            ."</div>";
+          
+   }
+}
+
+
